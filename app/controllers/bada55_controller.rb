42a08ab4902c122214931d963293577ea3250d38
Darrin Holst <darrinholst@gmail.com>
2013-03-03 10:56:39 -0600

    animations

--- app/controllers/bada55_controller.rb
+++ app/controllers/bada55_controller.rb
@@ -5,5 +5,33 @@ class Bada55Controller < UIViewController
     super
     self.title = TITLE
     view.backgroundColor = UIColor.bada55Color
+
+    @points = [[0, 0], [50, 0], [0, 50], [50, 50]]
+    @current_index = 0
+
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blackColor
+    view.addSubview(@view)
+
+    animate_to_next_point
+  end
+
+
+  def animate_to_next_point
+    @current_index += 1
+    @current_index = @current_index % @points.count
+
+    UIView.animateWithDuration(0.3,
+      delay: 1,
+      options: UIViewAnimationOptionCurveLinear,
+
+      animations: lambda {
+         @view.frame = [@points[@current_index], [100, 100]]
+      },
+
+      completion: lambda {|finished|
+        self.animate_to_next_point
+      }
+    )
   end
 end
